f96e73a78615d21f0580b81622f1c04934435941
Abhishek Bhargava <bhargavaabhishek2002@gmail.com>
2026-06-05 23:34:25 +0530

    fixed the changes

--- src/components/JobCard.jsx
+++ src/components/JobCard.jsx
@@ -2,20 +2,34 @@ import { Link } from "react-router-dom";
 
 function JobCard({ job, saveJob }) {
   return (
-    <div className="border rounded p-4 shadow">
-      <h2 className="text-xl fongit initt-bold text-indigo-600">{job.title}</h2>{" "}
-      <p className="mt-2">{job.company}</p>
+    <div className="border rounded-lg p-4 shadow bg-white text-black dark:bg-gray-800 dark:text-white">
+
+      <h2 className="text-xl font-bold">
+        {job.title}
+      </h2>
+
+      <p className="mt-2">
+        {job.company}
+      </p>
+
       <p>{job.location}</p>
+
       <p>{job.type}</p>
-      <Link to={`/job/${job.id}`} className="text-blue-500 block mt-2">
+
+      <Link
+        to={`/job/${job.id}`}
+        className="text-blue-500 block mt-2"
+      >
         View Details
       </Link>
+
       <button
         onClick={() => saveJob(job)}
         className="bg-green-500 text-white px-3 py-1 rounded mt-3"
       >
         Save Job
       </button>
+
     </div>
   );
 }
